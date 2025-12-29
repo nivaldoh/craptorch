ecb0eff5b24ccc42d38535b78d549c85e3c225b7
nivaldoh <nivaldo.humbertoo@gmail.com>
2025-12-29 00:43:18 -0300

    dropout, sequential

--- craptorch/core/layers.py
+++ craptorch/core/layers.py
@@ -52,3 +52,56 @@ class Linear(Layer):
     def __repr__(self):
         bias_str = f", bias={self.bias is not None}"
         return f"Linear(in_features={self.in_features}, out_features={self.out_features}{bias_str})"
+
+class Dropout(Layer):
+    def __init__(self, p=0.5):
+        if not DROPOUT_MIN_PROB <= p <= DROPOUT_MAX_PROB:
+            raise ValueError("invalid dropout prob")
+        self.p = p
+
+    def forward(self, x, training=True):
+        if not training or self.p == DROPOUT_MIN_PROB:
+            return x
+
+        if self.p == DROPOUT_MAX_PROB:
+            return Tensor(np.zeros_like(x.data), requires_grad=x.requires_grad)
+
+        keep_prob = 1.0 - p
+
+        mask = np.random.random(x.data.shape) < keep_prob
+
+        mask_tensor = Tensor(mask.astype(np.float32), requires_grad=False)
+        scale = Tensor(np.array(1.0/keep_prob), requires_grad=False)
+
+        return x * mask_tensor * scale
+
+    def __call__(self, x, training=True):
+        return self.forward(x, training)
+
+    def __repr__(self):
+        return f"Dropout(p={self.p})"
+
+class Sequential(Layer):
+    def __init__(self, *layers):
+        if len(layers) == 1 and isinstance(layers[0], (list, tuple)):
+            self.layers = list(layers[0])
+        else:
+            self.layers = layers
+
+    def forward(self, x):
+        for l in self.layer:
+            x = l.forward(x)
+        return x
+
+    def __call__(self, x):
+        self.forward(x)
+
+    def parameters(self):
+        p = []
+        for l in self.layers:
+            p.extend(l.parameters())
+        return p
+
+    def __repr__(self):
+        layer_reprs = ", ".join(repr(l) for l in self.layers)
+        return f"Sequential({layer_reprs})"

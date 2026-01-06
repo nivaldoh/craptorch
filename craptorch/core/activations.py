fix overflow

craptorch/core/activations.py
@@ -61,7 +61,8 @@ class Tanh (Activation):
 
 class GELU(Activation):
     def forward(self, x: Tensor):
-        sig = 1.0/(1.0 + np.exp(-1.702 * x.data))
+        z = np.clip(-1.702 * x.data, -500, 500)
+        sig = 1.0/(1.0 + np.exp(z))
         res = sig * x.data
         return Tensor(res)
     

craptorch/core/autograd.py
@@ -531,8 +531,7 @@ def enable_autograd(quiet=False):
     _original_ce_forward = CrossEntropyLoss.forward
 
     def tracked_sigmoid_forward(self, x):
-        result_data = 1.0/(1.0 + np.exp(-x.data))
-        result = Tensor(result_data)
+        result = _original_sigmoid_forward(self, x)
 
         if x.requires_grad:
             result.requires_grad = True